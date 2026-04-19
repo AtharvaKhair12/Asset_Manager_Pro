c5b4aa86acb4a0738a78e954ac5987eebbf6b061
AtharvaKhair12 <khairatharva441@gmail.com>
2026-04-19 13:23:28 +0530

    Fix email service method signature

--- asset-manager/src/main/java/com/nam/assetmanager/service/UserService.java
+++ asset-manager/src/main/java/com/nam/assetmanager/service/UserService.java
@@ -2,13 +2,10 @@ package com.nam.assetmanager.service;
 
 import com.nam.assetmanager.model.User;
 import com.nam.assetmanager.repositories.UserRepository;
-import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import org.springframework.transaction.annotation.Transactional;
-import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
 @Service
@@ -24,42 +21,29 @@ public class UserService {
     private EmailService emailService;
 
     @Transactional
-    public void saveUser(User user, String siteURL) throws MessagingException, UnsupportedEncodingException {
-        // 1. Encrypt the password using BCrypt for security
+    public void saveUser(User user, String siteURL) throws Exception {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-        // 2. Set default role for the Asset Management system
         user.setRole("ROLE_ADMIN");
-
-        // 3. Email Verification setup
         String randomCode = String.format("%06d", new Random().nextInt(1000000));
         user.setVerificationCode(randomCode);
         user.setEnabled(false);
-
-        // 4. Persist the user to the MySQL database
         userRepository.save(user);
-
-        // 5. Send Verification Email
-        emailService.sendVerificationEmail(user, siteURL);
+        emailService.sendVerificationEmail(user.getEmail(), user.getFullName(), user.getVerificationCode(), siteURL);
     }
 
     @Transactional
-    public void saveEmployee(User user, String siteURL) throws MessagingException, UnsupportedEncodingException {
+    public void saveEmployee(User user, String siteURL) throws Exception {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole("ROLE_EMPLOYEE");
-        
         String randomCode = String.format("%06d", new Random().nextInt(1000000));
         user.setVerificationCode(randomCode);
         user.setEnabled(false);
-        
         userRepository.save(user);
-        
-        emailService.sendVerificationEmail(user, siteURL);
+        emailService.sendVerificationEmail(user.getEmail(), user.getFullName(), user.getVerificationCode(), siteURL);
     }
 
     public boolean verify(String verificationCode) {
         User user = userRepository.findByVerificationCode(verificationCode);
-
         if (user == null || user.isEnabled()) {
             return false;
         } else {
